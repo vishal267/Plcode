432d2798bb7814a60ef13519aaa10a163739d876
vishal267 <vishal267@ymail.com>
2023-06-29 12:45:28 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,53 +1,115 @@
 from flask import Flask, jsonify, request
+import psycopg2
 import uuid
 
 app = Flask(__name__)
 
-# Sample data - messages
-messages = [
-    {
-        'account_id': '1',
-        'message_id': str(uuid.uuid4()),
-        'sender_number': '<SENDER_PHONE_NUMBER>',
-        'receiver_number': '<RECEIVER_PHONE_NUMBER>'
-    },
-    {
-        'account_id': '2',
-        'message_id': str(uuid.uuid4()),
-        'sender_number': '<SENDER_PHONE_NUMBER>',
-        'receiver_number': '<RECEIVER_PHONE_NUMBER>'
-    }
-]
-@app.route('/get/messages/account_id', methods=['GET'])
-def get_messages():
-    return jsonify(messages)
+def get_db_connection():
+    connection = psycopg2.connect(host='localhost',
+                            database='flask_db',
+                            user=os.environ['DB_USERNAME'],
+                            password=os.environ['DB_PASSWORD'])
+    return connection
 
-@app.route('/messages/<string:message_id>', methods=['GET'])
-def get_message(message_id):
-    message = next((message for message in messages if message['message_id'] == message_id), None)
-    if message:
-        return jsonify(message)
-    else:
-        return jsonify({'error': 'Message not found'}), 404
 
 # Route for creating a new message
 @app.route('/create', methods=['POST'])
 def create_message():
-    new_message = {
-        'account_id': request.json['account_id'],
-        'message_id': str(uuid.uuid4()),
-        'sender_number': request.json['sender_number'],
-        'receiver_number': request.json['receiver_number']
-    }
-    messages.append(new_message)
-    return jsonify(new_message), 201
+    try:
+        # Get message data from the request body
+        message_data = request.get_json()
+                
+        # Create a cursor to execute SQL queries
+        cursor = connection.cursor()
+        
+        # Insert the message into the database
+        query = """
+            INSERT INTO messages (account_id, message_id, sender_number, receiver_number)
+            VALUES (%s, %s, %s, %s)
+        """
+        cursor.execute(query, (
+            message_data['account_id'],
+            message_data['message_id'],
+            message_data['sender_number'],
+            message_data['receiver_number']
+        ))
+        
+        # Commit the changes and close the connection
+        connection.commit()
+        connection.close()
+        
+        return jsonify({'message': 'Message created successfully'})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
-# Route for deleting a message by message_id
-@app.route('/messages/<string:message_id>', methods=['DELETE'])
-def delete_message(message_id):
-    global messages
-    messages = [message for message in messages if message['message_id'] != message_id]
-    return jsonify({'message': 'Message deleted'})
+
+# Route for retrieving messages by account id 
+@app.route('/get/messages/<account_id>')
+def get_messages(account_id):
+    try:
+        # Create a cursor to execute SQL queries
+        cursor = connection.cursor()
+        
+        # Retrieve messages for the given account ID
+        query = """
+            SELECT * FROM messages WHERE account_id = %s
+        """
+        cursor.execute(query, (account_id,))
+        
+        # Fetch all rows and build the messages list
+        messages = []
+        rows = cursor.fetchall()
+        for row in rows:
+            message = {
+                'account_id': row[0],
+                'message_id': row[1],
+                'sender_number': row[2],
+                'receiver_number': row[3]
+            }
+            messages.append(message)
+        
+        # Close the connection
+        connection.close()
+        
+        return jsonify(messages)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+#Route with search cabapilities
+@app.route('/search')
+def search_messages():
+    try:
+        message_ids = request.args.get('message_id', '').split(',')
+
+        # Create a cursor to execute SQL queries
+        cursor = connection.cursor()
+
+        # Retrieve messages with the given message IDs
+        query = """
+            SELECT * FROM messages WHERE message_id IN %s
+        """
+        cursor.execute(query, (tuple(message_ids),))
+
+        # Fetch all rows and build the messages list
+        messages = []
+        rows = cursor.fetchall()
+        for row in rows:
+            message = {
+                'account_id': row[0],
+                'message_id': row[1],
+                'sender_number': row[2],
+                'receiver_number': row[3]
+            }
+            messages.append(message)
+
+        # Close the connection
+        connection.close()
+
+        return jsonify(messages)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 
 # Main entry point
 if __name__ == '__main__':
